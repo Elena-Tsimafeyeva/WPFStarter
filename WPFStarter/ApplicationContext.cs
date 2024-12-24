a4fe008f4564e9dfb71e79756e1d4633911b5da8
Elena-Tsimafeyeva <elenatsimafeyeva@gmail.com>
2024-12-25 00:05:19 +0300

    feat: Adding a People class to work with people data. Adding the ApplicationContext data context class. Adding a class Program for recording and receiving data.

---
+++ WPFStarter/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WPFStarter
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-2CDU469O;Database=People;Trusted_Connection=True;");
+        }
+    }
+}
