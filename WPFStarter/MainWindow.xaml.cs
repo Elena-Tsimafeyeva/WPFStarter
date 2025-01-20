ba8278a64deeec67c2b6744b1231273c37f33290
Elena-Tsimafeyeva <elenatsimafeyeva@gmail.com>
2025-01-20 23:18:51 +0300

    feat: Added StackPanel for RadioButtons. Added methods for the FileImport and FileExport buttons.

--- WPFStarter/MainWindow.xaml.cs
+++ WPFStarter/MainWindow.xaml.cs
@@ -19,6 +19,17 @@ namespace WPFStarter
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
