26f0067b02f0d183ad7611a012fedb0ba57f9044
Elena-Tsimafeyeva <elenatsimafeyeva@gmail.com>
2025-01-22 11:01:24 +0300

    feat: Added FileAvailability method to check for file existence. The size of TextBoxes for file paths have been edited.   A check for file availability has been added to the event for the import button. Added a check for data entry into the TextBox with Name "Data" to the event for the export button.

--- WPFStarter/MainWindow.xaml.cs
+++ WPFStarter/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WPFStarter
 {
@@ -21,15 +22,46 @@ namespace WPFStarter
             InitializeComponent();
 
         }
-
+        /// <summary>
+        /// E.A.T. 20-January-2025
+        /// Button event handler for import.
+        /// </summary>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            string? filePath = PathImport.Text;
+            FileAvailability(filePath);
         }
-
+        /// <summary>
+        /// E.A.T. 20-January-2025
+        /// Button event handler for export.
+        /// </summary>
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Экспорт данных");
+            string? text1 = Date.Text;
+            if (text1 != "") {
+                MessageBox.Show(text1);
+            }
+            else
+            {
+                MessageBox.Show("Date пустой");
+            }
+        }
+        /// <summary>
+        /// E.A.T. 22-January-2025
+        /// Checking for file availability.
+        /// </summary>
+        /// <param name="filePath"></param>
+        private static void FileAvailability(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                MessageBox.Show("Файл существует.");
+            }
+            else
+            {
+                MessageBox.Show("Файл не найден.");
+            }
         }
     }
 }
